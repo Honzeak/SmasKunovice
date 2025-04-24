b70b618d84e31a67e2dd410cce43acb381ffcbac
Jan Macek <honzamacek1@gmail.com>
2025-04-24 02:13:39 +0200

    Add GeoJSON assets

--- SmasKunovice.Avalonia/Views/MainView.axaml.cs
+++ SmasKunovice.Avalonia/Views/MainView.axaml.cs
@@ -1,6 +1,6 @@
-using Avalonia;
+using System.Collections.Generic;
+using System.IO;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
 using Mapsui.Tiling;
 
 namespace SmasKunovice.Avalonia.Views;
@@ -11,5 +11,16 @@ public partial class MainView : UserControl
     {
         InitializeComponent();
         MapControl.Map.Layers.Add(OpenStreetMap.CreateTileLayer());
+    }
+    
+    public static class GeoTiffFileProvider
+    {
+        public const string GeoTiffPath = @"C:\Users\honza\OneDrive\Code\SMAS-Data\Kunovice_tiff\";
+
+        public static IEnumerable<string> GetAllGeoTiffFiles()
+        {
+            return Directory.EnumerateFiles(GeoTiffPath, "*.tif", SearchOption.TopDirectoryOnly);
+        }
+        
     }
 }

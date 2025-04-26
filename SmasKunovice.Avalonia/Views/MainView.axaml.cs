5b52a62213c00884b9d2253c236e6f9da89675a0
Jan Macek <honzamacek1@gmail.com>
2025-04-26 11:52:55 +0200

    Mam in grid

--- SmasKunovice.Avalonia/Views/MainView.axaml.cs
+++ SmasKunovice.Avalonia/Views/MainView.axaml.cs
@@ -1,7 +1,14 @@
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Avalonia.Controls;
+using Avalonia.Media;
+using Mapsui;
+using Mapsui.Extensions.Provider;
+using Mapsui.Layers;
+using Mapsui.Styles;
 using Mapsui.Tiling;
+using Color = Mapsui.Styles.Color;
 
 namespace SmasKunovice.Avalonia.Views;
 
@@ -10,17 +17,39 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
-        MapControl.Map.Layers.Add(OpenStreetMap.CreateTileLayer());
+        CreateMap();
+        MapControl.Map = CreateMap();
+        MapControl.Map.Refresh();
     }
-    
+
+    private Map CreateMap()
+    {
+        var layers = GeoTiffFileProvider.GetAllGeoTiffFiles().Select(ILayer (x) =>
+        {
+            var provider = new GeoTiffProvider(x.filePath);
+            var layer = new Layer
+            {
+                DataSource = provider,
+                Name = x.fileName,
+                Style = new RasterStyle(),
+            };
+            return layer;
+        }).ToArray();
+        var map = new Map();
+        map.Layers.Add(layers);
+        return map;
+    }
+
     public static class GeoTiffFileProvider
     {
         public const string GeoTiffPath = @"C:\Users\honza\OneDrive\Code\SMAS-Data\Kunovice_tiff\";
 
-        public static IEnumerable<string> GetAllGeoTiffFiles()
+        public static IEnumerable<(string fileName, string filePath)> GetAllGeoTiffFiles()
         {
-            return Directory.EnumerateFiles(GeoTiffPath, "*.tif", SearchOption.TopDirectoryOnly);
+            return Directory.EnumerateFiles(GeoTiffPath, "*.tif", SearchOption.TopDirectoryOnly)
+                .Select(path => (Path.GetFileName(path), path));
         }
         
     }
+
 }

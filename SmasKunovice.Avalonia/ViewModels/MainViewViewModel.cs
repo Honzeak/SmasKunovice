0cf1999c8a99fcbf6a575390aaef324bfcdc0278
Jan Macek <honzamacek1@gmail.com>
2025-05-04 19:27:14 +0200

    Move map initialization from View to ViewModel

--- SmasKunovice.Avalonia/ViewModels/MainViewViewModel.cs
+++ SmasKunovice.Avalonia/ViewModels/MainViewViewModel.cs
@@ -1,18 +1,119 @@
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
-using System.Linq;
+using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using Mapsui;
 using Mapsui.Extensions.Provider;
 using Mapsui.Layers;
+using Mapsui.Nts;
 using Mapsui.Nts.Providers;
 using Mapsui.Styles;
+using Mapsui.Styles.Thematics;
 using Mapsui.Tiling.Layers;
+using SmasKunovice.Avalonia.Models;
 
 namespace SmasKunovice.Avalonia.ViewModels;
 
 public partial class MainViewViewModel : ViewModelBase
 {
-    [ObservableProperty]
-    private string _greeting = "Welcome to Avalonia!";
+    [ObservableProperty] private string _greeting = "Welcome to Avalonia!";
+
+    [ObservableProperty] private Map _map = new ();
+
+    public MainViewViewModel()
+    {
+        // Task.Run(async () => { Map = await CreateMapAsync(); });
+        Map = CreateMap();
+    }
+
+    private Map CreateMap()
+    {
+        var map = new Map();
+        try
+        {
+            map.CRS = "EPSG:5514";
+            map.Layers.Add(ZtmDynamicLayerFactory.CreateDynamicLayer(ZtmDatasets.ZTM100));
+            map.Layers.Add(CreateAirportElementsLayers());
+        }
+        catch (Exception e)
+        {
+            Greeting = e.Message;
+        }
+        return map;
+    }
+
+    private ILayer[] CreateAirportElementsLayers()
+    {
+        var geoJsonsDirPath = @"C:\Users\honza\codes\SmasKunovice\SmasKunovice.Avalonia\Assets\GeoJsonElements\";
+
+        List<ILayer> layers = [];
+        ILayer? runwayMarkingsLayer = null;
+        foreach (var path in Directory.GetFiles(geoJsonsDirPath, "*.geojson"))
+        {
+            var isRunwayMarkings = path.Contains("RunwayMarkings");
+            var geoJsonProvider = new GeoJsonProvider(path);
+
+            var layer = new Layer()
+            {
+                DataSource = geoJsonProvider,
+                Style = isRunwayMarkings ? new VectorStyle { Fill = new Brush(Color.White) } : CreateThemeStyle()
+            };
+            if (isRunwayMarkings)
+            {
+                runwayMarkingsLayer = layer;
+                continue;
+            }
+
+            layers.Add(layer);
+        }
+
+        if (runwayMarkingsLayer != null)
+            layers.Add(runwayMarkingsLayer);
+
+        return layers.ToArray();
+    }
+
+    // je to na picu, prepsat
+    private ThemeStyle CreateThemeStyle()
+    {
+        return new ThemeStyle(CreateVectorStyle);
+    }
+
+    private VectorStyle CreateVectorStyle(IFeature feature) => feature switch
+    {
+        GeometryFeature { Geometry: NetTopologySuite.Geometries.Point } => new SymbolStyle()
+        {
+            Fill = new Brush(new Color(245, 245, 242)),
+            SymbolScale = 0.3f
+        },
+        _ => new VectorStyle
+        {
+            Fill = new Brush(new Color(200, 200, 200)),
+        }
+    };
+
+    private static ILayer CreateGeoTiffLayer()
+    {
+        // var MbTilesFilePath = @"C:\Users\honza\OneDrive\Code\SMAS-Data\Kunovice_tiff\mbTiles\output_file.mbtiles";
+        // var mbTilesTileSource = new MbTilesTileSource(new SQLiteConnectionString(MbTilesFilePath, true));
+        // var mbTilesLayer = new TileLayer(mbTilesTileSource) { Name = "kundovice" };
+        // return mbTilesLayer;
+        var geotif = new GeoTiffProvider(@"C:\Users\honza\OneDrive\Code\SMAS-Data\Kunovice_tiff\0708D.tif",
+            new List<Color>()
+            {
+                Color.Aqua
+            });
+        var gifLayer = new Layer("nig") { DataSource = geotif };
+        var layer = new RasterizingTileLayer(gifLayer);
+        return gifLayer;
+    }
+
+    // public static class GeoJsonAirportStyleProvider
+    // {
+    //     public static VectorStyle GetStyle(GeoJsonProvider provider)
+    //     {
+    //         var extent = provider.GetExtent(
+    //     }
 }
--- SmasKunovice.Avalonia/Views/MainView.axaml.cs
+++ SmasKunovice.Avalonia/Views/MainView.axaml.cs
@@ -1,30 +1,6 @@
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Avalonia.Controls;
-using Avalonia.Media;
-using BruTile;
-using BruTile.MbTiles;
-using Mapsui;
-using Mapsui.ArcGIS;
-using Mapsui.ArcGIS.DynamicProvider;
-using Mapsui.Cache;
-using Mapsui.Extensions.Cache;
-using Mapsui.Extensions.Provider;
-using Mapsui.Layers;
-using Mapsui.Nts;
-using Mapsui.Nts.Providers;
-using Mapsui.Styles;
-using Mapsui.Styles.Thematics;
-using Mapsui.Tiling;
-using Mapsui.Tiling.Layers;
+using Avalonia.Logging;
 using SmasKunovice.Avalonia.ViewModels;
-using SQLite;
-using Brush = Mapsui.Styles.Brush;
-using Color = Mapsui.Styles.Color;
-using Pen = Mapsui.Styles.Pen;
 
 namespace SmasKunovice.Avalonia.Views;
 
@@ -34,106 +10,8 @@ public partial class MainView : UserControl
     {
         InitializeComponent();
         DataContext = new MainViewViewModel();
-        MapControl.Map = CreateMap();
-    }
-
-    private Map CreateMap()
-    {
-        var map = new Map();
-        map.CRS = "EPSG:5514";
-        map.Layers.Add(CreateDynamicLayer());
-        // Debugger.Launch();
-        map.Layers.Add(CreateAirportElementsLayers());
-        return map;
-    }
-
-    private ImageLayer CreateDynamicLayer()
-    {
-        IUrlPersistentCache? defaultCache = null;
-        var url = @"https://ags.cuzk.gov.cz/arcgis1/rest/services/ZTM/ZTM100/MapServer";
-        var capabilitiesHelper = new CapabilitiesHelper(defaultCache);
-        ArcGISDynamicCapabilities? capabilities = null;
-        capabilitiesHelper.CapabilitiesReceived += (sender, args) => capabilities = sender as ArcGISDynamicCapabilities;
-        capabilitiesHelper.GetCapabilities(url, CapabilitiesType.DynamicServiceCapabilities);
-        while (capabilities == null)
-        {
-            Task.Delay(100).ConfigureAwait(false);
-        }
+        MapControl.Map = ((MainViewViewModel)DataContext).Map;
         
-        var provider = new ArcGISDynamicProvider(url, capabilities, null, defaultCache){CRS = "EPSG:5514"};
-        return new ImageLayer { DataSource = provider };
     }
 
-    private ILayer[] CreateAirportElementsLayers()
-    {
-        var geoJsonsDirPath = @"C:\Users\honza\codes\SmasKunovice\SmasKunovice.Avalonia\Assets\GeoJsonElements\";
-
-        List<ILayer> layers = [];
-        ILayer? runwayMarkingsLayer = null;
-        foreach (var path in Directory.GetFiles(geoJsonsDirPath, "*.geojson"))
-        {
-            var isRunwayMarkings = path.Contains("RunwayMarkings");
-            var geoJsonProvider = new GeoJsonProvider(path);
-
-            var layer = new Layer()
-            {
-                DataSource = geoJsonProvider,
-                Style = isRunwayMarkings ? new VectorStyle { Fill = new Brush(Color.White) } : CreateThemeStyle()
-            };
-            if (isRunwayMarkings)
-            {
-                runwayMarkingsLayer = layer;
-                continue;
-            }
-
-            layers.Add(layer);
-        }
-
-        if (runwayMarkingsLayer != null)
-            layers.Add(runwayMarkingsLayer);
-
-        return layers.ToArray();
-    }
-
-    // je to na picu, prepsat
-    private ThemeStyle CreateThemeStyle()
-    {
-        return new ThemeStyle(CreateVectorStyle);
-    }
-
-    private VectorStyle CreateVectorStyle(IFeature feature) => feature switch
-    {
-        GeometryFeature { Geometry: NetTopologySuite.Geometries.Point } => new SymbolStyle()
-        {
-            Fill = new Brush(new Color(245, 245, 242)),
-            SymbolScale = 0.3f
-        },
-        _ => new VectorStyle
-        {
-            Fill = new Brush(new Color(200, 200, 200)),
-        }
-    };
-
-    private static ILayer CreateGeoTiffLayer()
-    {
-        // var MbTilesFilePath = @"C:\Users\honza\OneDrive\Code\SMAS-Data\Kunovice_tiff\mbTiles\output_file.mbtiles";
-        // var mbTilesTileSource = new MbTilesTileSource(new SQLiteConnectionString(MbTilesFilePath, true));
-        // var mbTilesLayer = new TileLayer(mbTilesTileSource) { Name = "kundovice" };
-        // return mbTilesLayer;
-        var geotif = new GeoTiffProvider(@"C:\Users\honza\OneDrive\Code\SMAS-Data\Kunovice_tiff\0708D.tif",
-            new List<Color>()
-            {
-                Color.Aqua
-            });
-        var gifLayer = new Layer("nig") { DataSource = geotif };
-        var layer = new RasterizingTileLayer(gifLayer);
-        return gifLayer;
-    }
-
-    // public static class GeoJsonAirportStyleProvider
-    // {
-    //     public static VectorStyle GetStyle(GeoJsonProvider provider)
-    //     {
-    //         var extent = provider.GetExtent(
-    //     }
 }

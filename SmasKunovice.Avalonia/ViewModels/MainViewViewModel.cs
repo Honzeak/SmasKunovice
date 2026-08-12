3d2c1e7a670652f6529d2a1e58137ff7c1d6535e
Jan Macek <honzamacek1@gmail.com>
2026-08-13 00:05:07 +0200

    Disable pan lock for debugging purposes

--- SmasKunovice.Avalonia/ViewModels/MainViewViewModel.cs
+++ SmasKunovice.Avalonia/ViewModels/MainViewViewModel.cs
@@ -103,12 +103,12 @@ public partial class MainViewViewModel : ViewModelBase, IDisposable
             map.CRS = "EPSG:5514";
             // Dark grey
             map.BackColor = MapsuiColor.FromString("#033052");
-            map.Navigator.OverridePanBounds = new MRect(
-                minX: -614991,
-                minY: -1227070,
-                maxX: -509776,
-                maxY: -1142803);
-            map.Navigator.Limiter = new ViewportLimiterKeepWithinExtent();
+            // map.Navigator.OverridePanBounds = new MRect( TODO enable after successful on-site deployment
+            //     minX: -614991,
+            //     minY: -1227070,
+            //     maxX: -509776,
+            //     maxY: -1142803);
+            // map.Navigator.Limiter = new ViewportLimiterKeepWithinExtent();
             AddLayers(map, MapLayerFactory.CreateZtmDynamicLayers(ZtmDatasets.ZTM100, ZtmDatasets.ZTM25));
             AddLayers(map, layerFactory.CreateAirportElementsLayers(_layerStyleProvider, out var procedureLayerNames).ToArray());
             foreach (var procedure in CreateProceduresModelList(procedureLayerNames))
